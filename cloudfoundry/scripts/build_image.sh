89db2fc2dbae837ae7cd5a042efd32b7b57b0985
Charles Moulliard <ch007m@gmail.com>
2021-03-12 09:38:14 +0100

    Improve the scriot to check if the cert file is there.

--- cloudfoundry/scripts/build_image.sh
+++ cloudfoundry/scripts/build_image.sh
@@ -4,19 +4,25 @@ REGISTRY_PROTOCOL=https
 REGISTRY_PREFIX=cmoulliard
 USER=admin
 PASSWORD=snowdrop
+buildImage=${1:-false}
+CERTIFICATE_FILE=../server.crt
 
-echo "IMPORTANT: Copy the self signed cert here please using the name : server.crt !!!!! before to launch the script"
+pushd tmp/cert-injection-webhook
 
-pushd cert-injection-webhook
-pack build $REGISTRY_PREFIX/my-setup-ca-certs  \
-    -e BP_GO_TARGETS="./cmd/setup-ca-certs"  \
-    --publish  \
-    --builder paketobuildpacks/builder:base
-pack build $REGISTRY_PREFIX/my-cert-webhook  \
-     -e BP_GO_TARGETS="./cmd/pod-webhook"  \
-     --publish
+echo "Build cert-webhook images: $buildImage"
+if $buildImage
+then
+  pack build $REGISTRY_PREFIX/my-setup-ca-certs  \
+      -e BP_GO_TARGETS="./cmd/setup-ca-certs"  \
+      --publish  \
+      --builder paketobuildpacks/builder:base
+  pack build $REGISTRY_PREFIX/my-cert-webhook  \
+       -e BP_GO_TARGETS="./cmd/pod-webhook"  \
+       --publish
+fi
 
-ytt -f ./deployments/k8s \
+if [ -f "$CERTIFICATE_FILE" ]; then
+    ytt -f ./deployments/k8s \
       -v pod_webhook_image=$REGISTRY_PREFIX/my-cert-webhook \
       -v setup_ca_certs_image=$REGISTRY_PREFIX/my-setup-ca-certs \
       --data-value-file ca_cert_data=../server.crt \
@@ -24,26 +30,36 @@ ytt -f ./deployments/k8s \
       --data-value-yaml annotations="[kpack.io/build]" \
       > manifest.yaml
 
-kapp delete -a cert-injection-webhook -y
-kapp deploy -a cert-injection-webhook -f ./manifest.yaml -y
+    kapp delete -a cert-injection-webhook -y
+    kapp deploy -a cert-injection-webhook -f ./manifest.yaml -y
+else
+    echo "#####################################################################################"
+    echo "IMPORTANT: The self signed file is missing !!"
+    echo "Copy it server.crt under the ./tmp directory please"
+    echo "#####################################################################################"
+    exit 0
+fi
+
 popd
 
-echo "Run the k8s cmds"
+echo "Download kpack release yaml file"
 rm -f release-0.2.2.yaml && wget https://github.com/pivotal/kpack/releases/download/v0.2.2/release-0.2.2.yaml
 
+echo "Install kpack"
 kubectl delete -f release-0.2.2.yaml
 kubectl apply -f release-0.2.2.yaml --validate=false
 
-cat <<EOF | kubectl apply -n kpack -f -
-apiVersion: v1
-kind: Secret
-metadata:
-  name: cert-key
-type: Opaque
-data:
-  server-key.pem: LS0tLS1CRUdJTiBFQyBQUklWQVRFIEtFWS0tLS0tCk1IY0NBUUVFSUw2VDRMNWtFT29RUXZBd2t5OEJIb1AySSsrTEtpSVBVZEF1MSsyb1hsWG9vQW9HQ0NxR1NNNDkKQXdFSG9VUURRZ0FFZkk5TCtwSlNVTjJkY2ovRlc4dnRGUHBmUG96T2VLT3lRZXBmeEorZEFJcTFJWllBdDF0MApGcWo1Q09ScFBSUUNmampvempRNDBxOXZBdlZpeUYzL0dnPT0KLS0tLS1FTkQgRUMgUFJJVkFURSBLRVktLS0tLQo=
-  server.crt: LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURGakNDQWY2Z0F3SUJBZ0lRUUdMR0hDall0d1FIR1hPaVlFdHozakFOQmdrcWhraUc5dzBCQVFzRkFEQVYKTVJNd0VRWURWUVFERXdwcmRXSmxjbTVsZEdWek1CNFhEVEl4TURNd09ERXpNalF4TkZvWERUSXlNRE13T0RFegpNalF4TkZvd2VURUxNQWtHQTFVRUJoTUNRa1V4RGpBTUJnTlZCQWdUQlU1aGJYVnlNUkl3RUFZRFZRUUhFd2xHCmJHOXlaVzV1WlhNeEd6QVpCZ05WQkFvVEVsSmxaQ0JJWVhRZ1RXbGtaR3hsZDJGeVpURVJNQThHQTFVRUN4TUkKVTI1dmQyUnliM0F4RmpBVUJnTlZCQU1URFd0MVltVXRjbVZuYVhOMGNua3dXVEFUQmdjcWhrak9QUUlCQmdncQpoa2pPUFFNQkJ3TkNBQVI4ajB2NmtsSlEzWjF5UDhWYnkrMFUrbDgrak01NG83SkI2bC9FbjUwQWlyVWhsZ0MzClczUVdxUGtJNUdrOUZBSitPT2pPTkRqU3IyOEM5V0xJWGY4YW80SElNSUhGTUE0R0ExVWREd0VCL3dRRUF3SUYKb0RBVEJnTlZIU1VFRERBS0JnZ3JCZ0VGQlFjREFUQU1CZ05WSFJNQkFmOEVBakFBTUlHUEJnTlZIUkVFZ1ljdwpnWVNDRjJ0MVltVXRjbVZuYVhOMGNua3VhVzVtY21FdWMzWmpnaDlyZFdKbExYSmxaMmx6ZEhKNUxtbHVabkpoCkxuTjJZeTVqYkhWemRHVnlnaVZyZFdKbExYSmxaMmx6ZEhKNUxtbHVabkpoTG5OMll5NWpiSFZ6ZEdWeUxteHYKWTJGc2doVTVOUzR5TVRjdU1UVTVMakkwTkM1dWFYQXVhVytIQkYvWm4vU0hCQXBpd0ZBd0RRWUpLb1pJaHZjTgpBUUVMQlFBRGdnRUJBSFNSMUJwcWZrQzJHUlFuMUFoRFlibDJNZXpCNHVqdDNERS8xSjlJdVFWR0VCTWRxSnEyClNSdEw0ZVVrKzVIL2J0OXdIVWc2MHZibnVQTHNBcURxejBDclZPeHJPZUZwdlJ0MXRyejFLR0RzVUlnbHBUUlIKVE9pWUhBQmRVYkdINUJhMEt1T01aWW10SEpwODBzbUVCcUFzcExCQVNIakEvOHVWbE9JYnlnTjRkSzUvUVROcQo2VWRNK1dNTVMrSzdQd0g3R0dlOHgxR0o3WHRZVVd4RFE4QmdycWtmUEFwM25TY201R1VId3ZIR3ZJZGg5cTZ4CnBFS201ejhmaDJwMFdyWXRPbXhRTmFaUHlwd0lGTmZETEYxeXBYdVkweHd0SlNvNXF3RHV6V3hsbEZmNSs3N2YKZkRJSzBvb2pPMXFES3c5ZlZraSt6b2E4V0pGbXN1NEJLWEU9Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0KLS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUN5RENDQWJDZ0F3SUJBZ0lCQURBTkJna3Foa2lHOXcwQkFRc0ZBREFWTVJNd0VRWURWUVFERXdwcmRXSmwKY201bGRHVnpNQjRYRFRJeE1ETXdPREE1TWpjME1Gb1hEVE14TURNd05qQTVNamMwTUZvd0ZURVRNQkVHQTFVRQpBeE1LYTNWaVpYSnVaWFJsY3pDQ0FTSXdEUVlKS29aSWh2Y05BUUVCQlFBRGdnRVBBRENDQVFvQ2dnRUJBTHhOCnVFSUc4TmtUV2MreHpFWFA4ZnpSSHNSQXBTOWdvRVBFYTVZYzk3bDNXZGcxbWErQ1ZVTlEzU1hqY0pUVkl3dEMKcHJ6OThKQjVhZHRRQ3JNUDZsOG5oQTJCWUowZUlneXZONTV0S0d6SmRJb3F0dFliUlVuK0V5dTNZUU1MU3ZveQpXUWRCelRkbUMraXV6dHZXY1MzM1dLdEczcHF2OU52d0Jxd3VEbDVIQ3NZWHFQWE9YeWFKY1gyd0JaSldWdEpsCjZlUS9pTDc3aDY3Ujhkd3J5enVwa2pzalV3SzBwZ2N0Z25HeC80S0c2YUJyeFhleWdmUUg5NEkxTjAzRHZpY2kKOEpIb2Mra3JUL3EyMWtqaUJEYXZ0UWQrNkkrRkNsZ3pybzZ3YWl0bFVHeWYrSjB3cjdMQmxzQUxtU05ZeFZHLwp3RWtlajc3RHdzbWVlM0dNSHNzQ0F3RUFBYU1qTUNFd0RnWURWUjBQQVFIL0JBUURBZ0trTUE4R0ExVWRFd0VCCi93UUZNQU1CQWY4d0RRWUpLb1pJaHZjTkFRRUxCUUFEZ2dFQkFKb2kxOXQ2YW1kNzFidkRJYmIyeUNHeGtvdVIKb0dZd2hxU3Z6Z0hQRmZuMSt2ckd1a1R5U3hjSTZsaGwrN0JjaVpkL1oramtVQ01xNDBHT1huV0N5QWE5SHBRQgoyWkdsZEhRdUplRnpJeTAxZUVMNUlDU3dMY2NDYzJtM2p3NmRVYnRnbldoUS9BalVZZno1OGZsTWFLUWhQbldqCkpmd1pSVW1rRm5jY25MR3ZPQzQxcTk2Zk9BazA5bWFiajlTZlduVEoydnBxdGJnTVRBTklVQjVtVkVZN3kzYXUKbG0zRjNFSGI2dkZLYklQcGVnSVZTWm1pV1dQNjVoZ2dUSFBnSmxCTkg1alBlRFdTSXk3bFBEMG9KREFvem1qZApzNGx0UlpyejNlZHdsR1U0SHNMc0dzeHZCNGJ3Yk16dkxDMHZEeFNtV1V3N085bXRONkowOHpkZWt1ST0KLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQo=
-EOF
+echo "Create the clusterStore, clusterStack, Builder for the demo"
+# cat <<EOF | kubectl apply -n kpack -f -
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#   name: cert-key
+# type: Opaque
+# data:
+#   server-key.pem: LS0tLS1CRUdJTiBFQyBQUklWQVRFIEtFWS0tLS0tCk1IY0NBUUVFSUw2VDRMNWtFT29RUXZBd2t5OEJIb1AySSsrTEtpSVBVZEF1MSsyb1hsWG9vQW9HQ0NxR1NNNDkKQXdFSG9VUURRZ0FFZkk5TCtwSlNVTjJkY2ovRlc4dnRGUHBmUG96T2VLT3lRZXBmeEorZEFJcTFJWllBdDF0MApGcWo1Q09ScFBSUUNmampvempRNDBxOXZBdlZpeUYzL0dnPT0KLS0tLS1FTkQgRUMgUFJJVkFURSBLRVktLS0tLQo=
+#   server.crt: LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURGakNDQWY2Z0F3SUJBZ0lRUUdMR0hDall0d1FIR1hPaVlFdHozakFOQmdrcWhraUc5dzBCQVFzRkFEQVYKTVJNd0VRWURWUVFERXdwcmRXSmxjbTVsZEdWek1CNFhEVEl4TURNd09ERXpNalF4TkZvWERUSXlNRE13T0RFegpNalF4TkZvd2VURUxNQWtHQTFVRUJoTUNRa1V4RGpBTUJnTlZCQWdUQlU1aGJYVnlNUkl3RUFZRFZRUUhFd2xHCmJHOXlaVzV1WlhNeEd6QVpCZ05WQkFvVEVsSmxaQ0JJWVhRZ1RXbGtaR3hsZDJGeVpURVJNQThHQTFVRUN4TUkKVTI1dmQyUnliM0F4RmpBVUJnTlZCQU1URFd0MVltVXRjbVZuYVhOMGNua3dXVEFUQmdjcWhrak9QUUlCQmdncQpoa2pPUFFNQkJ3TkNBQVI4ajB2NmtsSlEzWjF5UDhWYnkrMFUrbDgrak01NG83SkI2bC9FbjUwQWlyVWhsZ0MzClczUVdxUGtJNUdrOUZBSitPT2pPTkRqU3IyOEM5V0xJWGY4YW80SElNSUhGTUE0R0ExVWREd0VCL3dRRUF3SUYKb0RBVEJnTlZIU1VFRERBS0JnZ3JCZ0VGQlFjREFUQU1CZ05WSFJNQkFmOEVBakFBTUlHUEJnTlZIUkVFZ1ljdwpnWVNDRjJ0MVltVXRjbVZuYVhOMGNua3VhVzVtY21FdWMzWmpnaDlyZFdKbExYSmxaMmx6ZEhKNUxtbHVabkpoCkxuTjJZeTVqYkhWemRHVnlnaVZyZFdKbExYSmxaMmx6ZEhKNUxtbHVabkpoTG5OMll5NWpiSFZ6ZEdWeUxteHYKWTJGc2doVTVOUzR5TVRjdU1UVTVMakkwTkM1dWFYQXVhVytIQkYvWm4vU0hCQXBpd0ZBd0RRWUpLb1pJaHZjTgpBUUVMQlFBRGdnRUJBSFNSMUJwcWZrQzJHUlFuMUFoRFlibDJNZXpCNHVqdDNERS8xSjlJdVFWR0VCTWRxSnEyClNSdEw0ZVVrKzVIL2J0OXdIVWc2MHZibnVQTHNBcURxejBDclZPeHJPZUZwdlJ0MXRyejFLR0RzVUlnbHBUUlIKVE9pWUhBQmRVYkdINUJhMEt1T01aWW10SEpwODBzbUVCcUFzcExCQVNIakEvOHVWbE9JYnlnTjRkSzUvUVROcQo2VWRNK1dNTVMrSzdQd0g3R0dlOHgxR0o3WHRZVVd4RFE4QmdycWtmUEFwM25TY201R1VId3ZIR3ZJZGg5cTZ4CnBFS201ejhmaDJwMFdyWXRPbXhRTmFaUHlwd0lGTmZETEYxeXBYdVkweHd0SlNvNXF3RHV6V3hsbEZmNSs3N2YKZkRJSzBvb2pPMXFES3c5ZlZraSt6b2E4V0pGbXN1NEJLWEU9Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0KLS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUN5RENDQWJDZ0F3SUJBZ0lCQURBTkJna3Foa2lHOXcwQkFRc0ZBREFWTVJNd0VRWURWUVFERXdwcmRXSmwKY201bGRHVnpNQjRYRFRJeE1ETXdPREE1TWpjME1Gb1hEVE14TURNd05qQTVNamMwTUZvd0ZURVRNQkVHQTFVRQpBeE1LYTNWaVpYSnVaWFJsY3pDQ0FTSXdEUVlKS29aSWh2Y05BUUVCQlFBRGdnRVBBRENDQVFvQ2dnRUJBTHhOCnVFSUc4TmtUV2MreHpFWFA4ZnpSSHNSQXBTOWdvRVBFYTVZYzk3bDNXZGcxbWErQ1ZVTlEzU1hqY0pUVkl3dEMKcHJ6OThKQjVhZHRRQ3JNUDZsOG5oQTJCWUowZUlneXZONTV0S0d6SmRJb3F0dFliUlVuK0V5dTNZUU1MU3ZveQpXUWRCelRkbUMraXV6dHZXY1MzM1dLdEczcHF2OU52d0Jxd3VEbDVIQ3NZWHFQWE9YeWFKY1gyd0JaSldWdEpsCjZlUS9pTDc3aDY3Ujhkd3J5enVwa2pzalV3SzBwZ2N0Z25HeC80S0c2YUJyeFhleWdmUUg5NEkxTjAzRHZpY2kKOEpIb2Mra3JUL3EyMWtqaUJEYXZ0UWQrNkkrRkNsZ3pybzZ3YWl0bFVHeWYrSjB3cjdMQmxzQUxtU05ZeFZHLwp3RWtlajc3RHdzbWVlM0dNSHNzQ0F3RUFBYU1qTUNFd0RnWURWUjBQQVFIL0JBUURBZ0trTUE4R0ExVWRFd0VCCi93UUZNQU1CQWY4d0RRWUpLb1pJaHZjTkFRRUxCUUFEZ2dFQkFKb2kxOXQ2YW1kNzFidkRJYmIyeUNHeGtvdVIKb0dZd2hxU3Z6Z0hQRmZuMSt2ckd1a1R5U3hjSTZsaGwrN0JjaVpkL1oramtVQ01xNDBHT1huV0N5QWE5SHBRQgoyWkdsZEhRdUplRnpJeTAxZUVMNUlDU3dMY2NDYzJtM2p3NmRVYnRnbldoUS9BalVZZno1OGZsTWFLUWhQbldqCkpmd1pSVW1rRm5jY25MR3ZPQzQxcTk2Zk9BazA5bWFiajlTZlduVEoydnBxdGJnTVRBTklVQjVtVkVZN3kzYXUKbG0zRjNFSGI2dkZLYklQcGVnSVZTWm1pV1dQNjVoZ2dUSFBnSmxCTkg1alBlRFdTSXk3bFBEMG9KREFvem1qZApzNGx0UlpyejNlZHdsR1U0SHNMc0dzeHZCNGJ3Yk16dkxDMHZEeFNtV1V3N085bXRONkowOHpkZWt1ST0KLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQo=
+# EOF
 
 #kubectl delete deployment/kpack-controller -n kpack
 #cat <<EOF | kubectl apply -n kpack -f -
@@ -153,6 +169,7 @@ spec:
     image: "paketobuildpacks/run:base-cnb"
 EOF
 
+kubectl delete ns demo
 kubectl create ns demo
 
 kubectl delete sa/tutorial-service-account -n demo
@@ -215,3 +232,5 @@ spec:
       url: https://github.com/spring-projects/spring-petclinic
       revision: main
 EOF
+
+logs -namespace demo -image tutorial-image
