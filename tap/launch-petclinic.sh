52fcf1c543ead719782324fefc4c603cd4d572a6
Charles Moulliard <ch007m@gmail.com>
2021-09-20 21:15:54 +0200

    Move launch cmds to bash file

---
+++ tap/launch-petclinic.sh
@@ -0,0 +1,14 @@
+export KUBECONFIG=$HOME/.kube/h01-121
+export VM_IP=95.217.159.244
+
+export UI_NODE_PORT=$(kubectl get svc/acc-ui-server -n accelerator-system -o jsonpath='{.spec.ports[0].nodePort}')
+echo "Accelerator UI: http://$VM_IP:$UI_NODE_PORT"
+open -na "Google Chrome" --args --incognito http://$VM_IP:$UI_NODE_PORT
+
+export LIVE_NODE_PORT=$(kubectl get svc/application-live-view-5112 -n tap-install -o jsonpath='{.spec.ports[0].nodePort}')
+echo "Live view: http://$VM_IP.nip.io:$LIVE_NODE_PORT/apps"
+open -na "Google Chrome" --args --incognito http://$VM_IP.nip.io:$LIVE_NODE_PORT/apps
+
+export ENVOY_NODE_PORT=$(kubectl get svc/envoy -n contour-external -o jsonpath='{.spec.ports[0].nodePort}')
+echo "Petclinic demo: http://petclinic.tap-install.$VM_IP.nip.io:$ENVOY_NODE_PORT"
+open -na "Google Chrome" --args --incognito http://petclinic.tap-install.$VM_IP.nip.io:$ENVOY_NODE_PORT
