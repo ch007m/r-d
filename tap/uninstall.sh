bc64b609c84385e78448fe3d3395425249c4c972
Charles Moulliard <cmoulliard@redhat.com>
2022-02-01 13:42:58 +0100

    Review the steps to clean up a TAP installation

--- tap/uninstall.sh
+++ tap/uninstall.sh
@@ -1,21 +1,54 @@
+#
+# Execute this command remotely
+# ssh -i <PUB_KEY_FILE_PATH> <USER>@<IP> -p <PORT> "bash -s" -- < ./uninstall.sh
+#
+# Define the following env vars:
+# - REMOTE_HOME_DIR: home directory where files will be installed within the remote VM
+#
 
-KUBE_CFG_FILE=${1:-h01-121}
+KUBE_CFG_FILE=${1:-config}
 export KUBECONFIG=$HOME/.kube/${KUBE_CFG_FILE}
 
-kubectl delete secret tap-registry -n tap-install
+NAMESPACE="tap-install"
+NAMESPACE_DEMO="tap-demo"
 
-declare -a packages=("app-accelerator" "app-live-view" "cloud-native-runtimes")
+REMOTE_HOME_DIR="/home/snowdrop"
+DEST_DIR="/usr/local/bin"
+TANZU_TEMP_DIR="$REMOTE_HOME_DIR/tanzu"
+
+tanzu apps workload list -n $NAMESPACE_DEMO | awk '(NR>1)' | while read name app status age;
+do
+  echo "Deleting the $name workload under $NAMESPACE_DEMO"
+  tanzu -n $NAMESPACE_DEMO apps workload delete $name --yes
+done
+kubectl delete ns $NAMESPACE_DEMO
+
+while read -r package; do
+  name=$(echo $package | jq -r '.name')
+  repo=$(echo $package | jq -r '.repository')
+  tag=$(echo $package | jq -r '.tag')
+  echo "Deleting the package: $name"
+  tanzu package installed delete $name -n $NAMESPACE -y
+done <<< "$(tanzu package installed list -n $NAMESPACE -o json | jq -c '.[]')"
+
+while read -r package; do
+  name=$(echo $package | jq -r '.name')
+  repo=$(echo $package | jq -r '.repository')
+  tag=$(echo $package | jq -r '.tag')
+  echo "Deleting the repository: $name"
+  tanzu package repository delete $name -n $NAMESPACE -y
+done <<< "$(tanzu package repository list -n $NAMESPACE -o json | jq -c '.[]')"
+
+declare -a packages=("tap-install" "kapp-controller" "secretgen-controller" "tanzu-cluster-essentials"  "tanzu-package-repo-global")
 for pkg in ${packages[@]}; do
-  echo "### DELETING A TANZU PACKAGE####"
-  echo "tanzu package installed delete $pkg -n tap-install -y"
-  tanzu package installed delete $pkg -n tap-install -y
+   echo "Deleting the resources and namespace of: $pkg"
+   kubectl delete all --all -n $pkg
+   kubectl delete ns  $pkg
 done
 
-declare -a packages=("tap-service-account" "tanzu-build-service" "tap-package-repo" "flux" "kc")
-for pkg in ${packages[@]}; do
-  echo "### DELETING ####"
-  echo "kapp delete -a $pkg -y"
-  kapp delete -a $pkg -y
-done
-
-kubectl delete ns tap-install
+echo "## Clean previous installation of the Tanzu client"
+rm -rf $TANZU_TEMP_DIR/cli    # Remove previously downloaded cli files
+sudo rm /usr/local/bin/tanzu  # Remove CLI binary (executable)
+rm -rf ~/.config/tanzu/       # current location # Remove config directory
+rm -rf ~/.tanzu/              # old location # Remove config directory
+rm -rf ~/.cache/tanzu         # remove cached catalog.yaml
